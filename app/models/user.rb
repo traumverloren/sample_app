59071cc95979ad757a1ec22aa138051239eddbf1
Stephanie Nemeth <traumverloren@gmail.com>
2015-03-16 17:39:25 +0100

    added bcrypt, password validation. finished 6.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+
+  has_secure_password
 end
