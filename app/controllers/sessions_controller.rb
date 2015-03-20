a3bbbddf700d12cc9734cdd8bbc0502ef1dcb2a8
Stephanie Nemeth <traumverloren@gmail.com>
2015-03-20 18:35:50 +0100

    finished 8.2.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
       flash[:notice] = "Welcome!"
     else
       flash.now[:danger] = "Sorry, please try again."
@@ -13,6 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    redirect_to root_path
   end
 
 end
