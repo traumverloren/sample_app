4f81c4fec4f313b47079ac6664b83c07a45e665d
Stephanie Nemeth <traumverloren@gmail.com>
2015-03-27 08:19:01 +0100

    finished ch8 ex1

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,16 +12,18 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+class << self
   # Returns the hash digest of the given string
-  def self.digest(string)
+  def digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token
-  def self.new_token
+  def new_token
     SecureRandom.urlsafe_base64
   end
+end
 
   def remember
     self.remember_token = User.new_token
