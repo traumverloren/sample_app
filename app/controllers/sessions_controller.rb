1a0ab0c8c2d6f13f2c9b8b1d30c1b23c88559426
Stephanie Nemeth <traumverloren@gmail.com>
2015-03-31 08:29:27 +0200

    finished 9.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
-      flash[:notice] = "Welcome!"
+      redirect_back_or user
     else
       flash.now[:danger] = "Sorry, please try again."
       render 'new'
